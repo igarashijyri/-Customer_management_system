5f6803aadfdf246aa660485320b53b7c5feba719
sho <fourleafclover688@gmail.com>
2021-02-28 09:03:24 +0000

    fix: スペルミスなどバグ修正

--- config/application.rb
+++ config/application.rb
@@ -8,18 +8,18 @@ module Baukis2
   class Application < Rails::Application
     config.load_defaults 6.0
 
-    config.time_zone = "Toyko"
-    config.i18n.load.path +=
+    config.time_zone = "Tokyo"
+    config.i18n.load_path +=
       Dir[Rails.root.join("cofig", "locales", "**", "*.{rb.yml}").to_s]
     config.i18n.default_locale = :ja
-  end
 
-  config.generators do |g|
-    g.skip_routes true
-    g.helper false
-    g.assets false
-    g.test_framework :rspec
-    g.controller_specs false
-    g.view_specs false
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
   end
 end
