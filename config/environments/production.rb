35af02d27a95a7625d957cb3d02e44a75b063ecd
sho <fourleafclover688@gmail.com>
2021-03-17 06:12:56 +0000

    feature: productionの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.hosts << "localhost" # 一旦こうしておくけど多分間違ってるから見直したほうがいい
 end
