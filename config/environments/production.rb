20320bed4344c73d227283184be9bf3b27f580e5
sho <fourleafclover688@gmail.com>
2021-03-18 09:23:34 +0000

    feature: production以外ではデフォルトのエラー画面を出すように変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = false # falseでアプリを再起動しなくてもコード変更が反映されるようになる？っぽい。多分。
+  config.cache_classes = true # falseでアプリを再起動しなくてもコード変更が反映されるようになる？っぽい。多分。
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
