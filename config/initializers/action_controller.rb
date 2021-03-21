7cd611c76b11f78eb48f572ee742a044daa17ccc
sho <fourleafclover688@gmail.com>
2021-03-21 15:55:39 +0000

    feature: strong_parameterの無効化ができていなかったので修正

--- config/initializers/action_controller.rb
+++ config/initializers/action_controller.rb
@@ -1,4 +1,4 @@
 # TODO: 一時的に外しているだけなので最後には消すこと
 Rails.application.configure do
-    config.action_controller.permit_all_parameter = true
-  end
+  config.action_controller.permit_all_parameters = true
+end
