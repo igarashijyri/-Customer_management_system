4bddb7b91f01d138dabb96b3d35d294225590050
sho <fourleafclover688@gmail.com>
2021-03-21 15:57:19 +0000

    featuer: ログインフォームを追加

---
+++ app/controllers/staff/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Staff::SessionsController < Staff::Base
+  def new
+    if current_staff_member
+      redirect_to :staff_root
+    else
+      @form = Staff::LoginForm.new
+      render action: "new"
+    end
+  end
+
+  def create
+    @form = Staff::LoginForm.new(params[:staff_login_form])
+
+    # TODO: Controllerにロジック書いてるの良くないのでモデルに書き出したい
+    if @form.email.present?
+      staff_member = StaffMember.find_by("LOWER(email) = ?", @form.email.downcase) 
+    end
+
+    # TODO: フォームからemailの値が送られなかった場合にundifind_method_errorになると思われるので、確認&修正が必要
+    if staff_member
+      session[:staff_member_id] = staff_member.id
+      redirect_to :staff_root
+    else
+      render action: "new"
+    end
+  end
+end
