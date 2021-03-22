58c3e6fe91fc944ecd06532adafa8b1e720b077a
sho <fourleafclover688@gmail.com>
2021-03-22 09:45:02 +0000

    test: ログインのテスト実装

--- spec/factories/staff_members.rb
+++ spec/factories/staff_members.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :staff_member do
-    
+    sequence(:email) { |n| "member#{n}@example.com"}
+    familiy_name { "山田" }
+    given_name { "太郎" }
+    familiy_name_kana { "ヤマダ" }
+    given_name_kana { "タロウ" }
+    password { "password" }
+    start_date { Date.yesterday }
+    end_date { nil }
+    suspended { false }
   end
 end
---
+++ spec/services/staff/authenticator_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe Staff::Authenticator do
+  describe "#authenticate" do
+
+    example "正しいパスワードのとき、trueを返す" do
+      staff_member = build(:staff_member)
+      expect(Staff::Authenticator.new(staff_member).authenticate("password")).to be_truthy
+    end
+
+    example "誤ったパスワードのとき、falseを返す" do
+      staff_member = build(:staff_member)
+      expect(Staff::Authenticator.new(staff_member).authenticate("pw")).to be_falsey
+    end
+
+    example "パスワードが未設定のとき、falseを返す" do
+      staff_member = build(:staff_member, password: nil)
+      expect(Staff::Authenticator.new(staff_member).authenticate(nil)).to be_falsey
+    end
+
+    example "停止フラグが立っているとき、falseを返す" do
+      staff_member = build(:staff_member, suspended: true)
+      expect(Staff::Authenticator.new(staff_member).authenticate(nil)).to be_falsey
+    end
+
+    example "開始前のとき、falseを返す" do
+      staff_member =  build(:staff_member, start_date: Date.tomorrow)
+      expect(Staff::Authenticator.new(staff_member).authenticate(nil)).to be_falsey
+    end
+
+    example "終了後のとき、falseを返す" do
+      staff_member = build(:staff_member, end_date: Date.today)
+      expect(Staff::Authenticator.new(staff_member).authenticate(nil)).to be_falsey
+    end
+  end
+end
