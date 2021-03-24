62fae491db61612829cd3f07f63bb5da8ab9091c
sho <fourleafclover688@gmail.com>
2021-03-24 09:20:42 +0000

    feature: ホスト制約の追加

--- config/initializers/baukis2.rb
+++ config/initializers/baukis2.rb
@@ -1,7 +1,7 @@
-Rails.aaplication.configure do
+Rails.application.configure do
   config.baukis2 = {
-    staff: { host: "baukis2.example.com", path: ""}
-    admin: { host: "baukis2.example.com", path: "admin"}
+    staff: { host: "baukis2.example.com", path: ""},
+    admin: { host: "baukis2.example.com", path: "admin"},
     customer: { host: "example.com", path: "mypage"}
   }
 end
