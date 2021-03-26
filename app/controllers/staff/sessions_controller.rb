1e6a3d32a6f924dfaaee059421e17e6abb3db9b4
sho <fourleafclover688@gmail.com>
2021-03-26 10:20:06 +0000

    fix: レイアウト壊れてたりする箇所とか修正

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -9,14 +9,13 @@ class Staff::SessionsController < Staff::Base
   end
 
   def create
-    @form = Staff::LoginForm.new(params[:staff_login_form])
+    @form = Staff::LoginForm.new(login_form_params)
 
     # TODO: Controllerにロジック書いてるの良くないのでモデルに書き出したい
     if @form.email.present?
       staff_member = StaffMember.find_by("LOWER(email) = ?", @form.email.downcase) 
     end
 
-    # TODO: フォームからemailの値が送られなかった場合にundifind_method_errorになると思われるので、確認&修正が必要
     # TODO: このif文、意図がわかりづらいからStaff::Authenticatorと合わせて修正したい
     if Staff::Authenticator.new(staff_member).authenticate(@form.password)
       if staff_member.suspended?
@@ -42,6 +41,6 @@ class Staff::SessionsController < Staff::Base
   private
 
   def login_form_params
-    params.require(:staff_login_form).permit(:email, :passwword)
+    params.require(:staff_login_form).permit(:email, :password)
   end
 end
