ee34a67651f78829304903d1cbdbb38730b4981b
sho <fourleafclover688@gmail.com>
2021-04-01 17:12:16 +0000

    feature: 国際対応の追加

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -2,7 +2,7 @@ class FormPresenter
   include HtmlBuilder
   
   attr_reader :form_builder, :view_context
-  delegate :label, :text_field, :date_field, password_field,
+  delegate :label, :text_field, :date_field, :password_field,
     :check_box, :radio_button, :text_area, :object, to: :form_builder
 
   def initialize(form_builder, view_context)
@@ -21,12 +21,15 @@ class FormPresenter
     markup(:div, class: "input-block") do |m|
       m << decorated_label(name, label_text, options)
       m << text_field(name, options)
+      m << error_messages_for(name)
+    end
   end
 
   def password_field_block(name,label_text, options = {})
     markup(:div, class: "input-block") do |m|
       m << decorated_label(name, label_text, options)
       m << password_field(name, options)
+      m << error_messages_for(name)
     end
   end
 
@@ -34,6 +37,17 @@ class FormPresenter
     markup(:div, class: "input-block") do |m|
       m << decorated_label(name, label_text, options)
       m << date_field(name, options)
+      m << error_messages_for(name)
+    end
+  end
+
+  def error_messages_for(name)
+    markup do |m|
+      object.errors.full_messages_for(name).each do |message|
+        m.div(class: "error_message") do |m|
+          m.text message
+        end
+      end
     end
   end
 
--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,7 @@ module Baukis2
     config.load_defaults 6.0
 
     config.time_zone = "Tokyo"
-    config.i18n.load_path +=
-      Dir[Rails.root.join("cofig", "locales", "**", "*.{rb.yml}").to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
     config.generators do |g|
