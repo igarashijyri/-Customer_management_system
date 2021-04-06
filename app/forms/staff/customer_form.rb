7033a0b57be74404dc6d4e65966569f4131c3818
sho <fourleafclover688@gmail.com>
2021-04-06 15:19:01 +0000

    feature: 編集画面の足りてないところ追加

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -1,12 +1,13 @@
-class Staff::CustomeForm
+class Staff::CustomerForm
   include ActiveModel::Model
 
   attr_accessor :customer
   delegate :persited?, to: :customer
 
-  def initialize
+  def initialize(customer = nil)
     @customer = customer
     @customer ||= Customer.new(gender: "male")
     @customer.build_home_address unless @customer.home_address
     @customer.build_work_address unless @customer.work_address 
   end
+end
